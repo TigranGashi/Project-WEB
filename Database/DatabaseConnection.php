c74f753dfd32c58e3ac828b4384a52a3b882fef8
TigranGashi <93322726+TigranGashi@users.noreply.github.com>
2023-01-25 21:30:40 +0100

    Update DatabaseConnection.php

--- Database/DatabaseConnection.php
+++ Database/DatabaseConnection.php
@@ -1,4 +1,24 @@
 <?php
 
+class DataBaseConnection{
+    private $server = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "Tech14";
 
+    function startConnection(){
+        try{
+            //Koneksioni PDO-(PHP DATA OBJECT)
+            $conn = new PDO("mysql:host=$this->server; dbname=$this->database, $this->username, $this->password");
+            //PDO error
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }catch(PDOException $e){
+            //Mesazhi
+            echo "Database Connection Failed" .$e->getMessage();
+            return null;
+        }
+
+    }
+}
 ?>
